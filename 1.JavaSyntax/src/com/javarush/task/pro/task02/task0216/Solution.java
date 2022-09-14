d32ab3460b051d33eec9fe7a26aed4fadee774bf
OOV <oshchepkov1911@mail.ru>
2022-09-14 18:16:14 +0300

    add solution JavaSyntax from task 0117

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0216/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task02.task0216;
+
+/* 
+Генератор позитивных новостей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Oleg", date = "14.09.2022.", time = "12;34", noun="dthbth", adjective="kxdfjhg", verb="hfhfghbgv";
+
+        System.out.println("Новости:");
+        System.out.println(name + " " + date + " " + time + " изобрел " + adjective + " " + noun + " который " + verb + " террабайты данных в минуту.");
+    }
+}

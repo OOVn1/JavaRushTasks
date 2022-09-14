0e7b3ebd8920e43576d8f13a1038e5903546b11b
OOV <oshchepkov1911@mail.ru>
2022-09-14 18:09:08 +0300

    add solution JavaSyntax from task 0116

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.pro.task02.task0205;
+
+/* 
+Когда-то было 32 зуба
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int toothCounter = 32;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+        toothCounter--;
+
+        //напишите тут ваш код
+
+
+        System.out.println(toothCounter);
+    }
+}

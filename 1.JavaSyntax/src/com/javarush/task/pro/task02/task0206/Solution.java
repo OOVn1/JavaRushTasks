dd60ce3605a82c2cf436d20c43a05e7c9ec02d08
OOV <oshchepkov1911@mail.ru>
2022-09-14 18:21:11 +0300

    add solution JavaSyntax from task 0118

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word="12",phrase="edf", line="frgar", text="2134";
+    }
+}

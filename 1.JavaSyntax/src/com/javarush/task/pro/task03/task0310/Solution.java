5f1056414244d43b10a5c7118b46a93f99904d2b
OOV <oshchepkov1911@mail.ru>
2022-09-19 20:01:19 +0300

    add solution JavaSyntax from task 0143

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0310/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task03.task0310;
+
+import java.util.Scanner;
+
+/* 
+Через тернии...
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number < 5) System.out.println("число меньше 5");
+        if (number == 5) System.out.println("число равно 5");
+        if (number > 5) System.out.println("число больше 5");
+
+    }
+}

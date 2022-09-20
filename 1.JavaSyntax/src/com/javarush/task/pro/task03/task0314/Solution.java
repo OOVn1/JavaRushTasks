410a0853734fa0a0f4e1f4cff8c3c37cb2683740
OOV <oshchepkov1911@mail.ru>
2022-09-20 21:14:02 +0300

    add solution JavaSyntax from task 0148

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String stroka = scanner.nextLine();
+        if (stroka.equalsIgnoreCase(secret))
+        System.out.println("доступ разрешен");
+        else
+        System.out.println("доступ запрещен");
+    }
+}

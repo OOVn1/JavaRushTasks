ac34b099ff4a5bb236da4faa102f31b4c5311e46
OOV <oshchepkov1911@mail.ru>
2022-09-20 21:59:51 +0300

    add solution JavaSyntax from task 0153

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 0;
+        while(a<5){
+            int b =0;
+            while(b<10){
+                System.out.print("Q");
+                b++;
+            }
+            System.out.println();
+            a++;
+        }
+
+    }
+}

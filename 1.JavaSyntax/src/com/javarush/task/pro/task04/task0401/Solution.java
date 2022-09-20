d95a870b623baa9d2149ae5768cf551871a6df54
OOV <oshchepkov1911@mail.ru>
2022-09-20 21:22:48 +0300

    add solution JavaSyntax from task 0150

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int a = 100;
+        while(a>0){
+            System.out.println(quote);
+            a--;
+        }
+
+    }
+}

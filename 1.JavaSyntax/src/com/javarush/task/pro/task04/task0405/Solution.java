1ff73f07c137e45714ef72f3fff6f0de40e9d5fd
OOV <oshchepkov1911@mail.ru>
2022-09-21 14:11:02 +0300

    add solution JavaSyntax from task 0154

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}

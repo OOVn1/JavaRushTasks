af8f4a1c666b5ccc20adfd51a7cf9f3aeb955e43
OOV <oshchepkov1911@mail.ru>
2022-09-22 22:08:16 +0300

    add solution JavaSyntax from task 0159

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0411/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task04.task0411;
+
+/* 
+Четные числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for(int a = 1; a < 15; a++){
+            if ( a % 2 ==0) System.out.println(a);
+        }
+
+    }
+}

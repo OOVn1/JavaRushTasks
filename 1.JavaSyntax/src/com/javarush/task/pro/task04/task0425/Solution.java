362da00d7764638bc4d3b588211ce39b49bc8a08
OOV <oshchepkov1911@mail.ru>
2022-09-22 22:50:54 +0300

    add solution JavaSyntax from task 0164

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0425/Solution.java
@@ -14,8 +14,18 @@ public class Solution {
 
     public static void main(String[] args) {
         for (int a = 0; a < 10; a ++){
-            for (int s = 0; s < 10; s++){
-                System.out.print(land);
+            if (a == 9){
+                for(int i = 0; i<10; i++) {
+                    System.out.print(bug);
+                }
+            }
+            else {
+                for (int s = 0; s < 10; s++) {
+                    if (a == 0 && s == 3) {
+                        System.out.print(robot);
+                    } else
+                        System.out.print(land);
+                }
             }
             System.out.println();
         }

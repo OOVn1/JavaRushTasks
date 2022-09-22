747182b3743572b1f65932d7b6dfda7a8a9581e7
OOV <oshchepkov1911@mail.ru>
2022-09-22 22:36:00 +0300

    add solution JavaSyntax from task 0162

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0424/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0424;
+
+import java.util.Scanner;
+
+/* 
+Бомба в ядре планеты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner bombInterface = new Scanner(System.in);
+        int a = 0;
+        do{
+            a = bombInterface.nextInt();
+            System.out.println(a);
+        }
+        while (a % 10 != 0);
+
+
+    }
+}

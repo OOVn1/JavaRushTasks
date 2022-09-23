7ddea5ecc072e2f87e9cf194678aae3b6448cc8e
OOV <oshchepkov1911@mail.ru>
2022-09-23 18:40:34 +0300

    add solution JavaSyntax from task 0170

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -6,7 +6,23 @@ package com.javarush.task.pro.task04.task0405;
 
 public class Solution {
     public static void main(String[] args) {
-
-
+        int i = 0;
+        while (i < 10) {
+            int j = 0;
+            while (j < 20) {
+                if( i == 0 || i == 9) {
+                    System.out.print("Б");
+                }
+                else if ( j == 0 || j ==19){
+                    System.out.print("Б");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
     }
 }

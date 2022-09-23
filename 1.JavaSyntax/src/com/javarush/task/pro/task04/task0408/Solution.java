994c05a61e76ad4fa76b996f2cf0adcd53077781
OOV <oshchepkov1911@mail.ru>
2022-09-23 18:58:56 +0300

    add solution JavaSyntax from task 0171

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -8,7 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner  (System.in);
+        int max = Integer.MIN_VALUE;
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            if( number % 2 == 0 && number > max){
+                max = number;
+            }
+        }
 
+        System.out.println(max);
     }
 }

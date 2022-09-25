d63f8f8d4e97d06b89496ebe0aa76c1f3d7c35b4
OOV <oshchepkov1911@mail.ru>
2022-09-25 13:44:38 +0300

    add solution JavaSyntax from task 0502

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task05.task0503;
+
+/* 
+Работаем с элементами массива
+*/
+
+public class Solution {
+
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}

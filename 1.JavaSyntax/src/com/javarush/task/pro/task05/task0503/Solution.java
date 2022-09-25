60edd413670c4d5bb84ede97f42a4549622841c0
OOV <oshchepkov1911@mail.ru>
2022-09-25 13:53:15 +0300

    add solution JavaSyntax from task 0503

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0503/Solution.java
@@ -9,7 +9,9 @@ public class Solution {
     public static int[] array = new int[]{-1, 2, 3, -4, -5};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < array.length; i++){
+            if( array[i] % 2 == 0) array[i] = array[i] * -1;
+        }
         System.out.println(array[0]);
         System.out.println(array[1]);
         System.out.println(array[2]);

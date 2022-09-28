b30da4fe412ecb3c2eaf04c66325edd9e3f1176a
OOV <oshchepkov1911@mail.ru>
2022-09-28 17:04:20 +0300

    add solution JavaSyntax from task 0509

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -9,6 +9,13 @@ public class Solution {
     public static int[][] MULTIPLICATION_TABLE;
 
     public static void main(String[] args) {
-        MULTIPLICATION_TABLE = new int [10][10];
+        MULTIPLICATION_TABLE = new int[10][10];
+        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
+            for (int j = 0; j < MULTIPLICATION_TABLE.length; j++) {
+                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

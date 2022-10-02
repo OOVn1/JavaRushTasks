47724b5ba15f1fe57d8351844728ed145987b7b0
OOV <oshchepkov1911@mail.ru>
2022-10-02 15:48:29 +0300

    add solution JavaSyntax from task 0511

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task05.task0511;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -10,12 +11,19 @@ public class Solution {
     public static int[][] multiArray;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        int N = scanner.nextInt();
-        multiArray = new int[N][];
-        for(int i = 0; i < N; i++){
-
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        multiArray = new int[number][];
+        for (int i = 0; i < number; i++) {
+            int num = scanner.nextInt();
+            multiArray[i] = new int[num];
         }
-
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                multiArray[i][j] = 0;
+                System.out.print(multiArray[i][j]);
+            }
+            System.out.println();
+        }
     }
 }

5ace2603a747c599b425026d2e6eabfb2d3e2b88
OOV <oshchepkov1911@mail.ru>
2022-10-02 16:09:41 +0300

    add solution JavaSyntax from task 0528

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -3,12 +3,15 @@ package com.javarush.task.pro.task05.task0510;
 /* 
 Треугольный массив
 */
-
+/*
 public class Solution {
 
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        array[0] = new int[1];
+        array[1] = new int[2];
+        array[2] = new int[3];
     }
 }
+*/
--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0528/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0528/Solution.java
@@ -7,18 +7,19 @@ import java.util.Arrays;
 */
 
 public class Solution {
-    public static int[][] rockets = {{0,1,1,1,0,0,0,1},{1,0,0,0,1,1,1,0,1,1},{1,0,1,1,0,0,1,1,1,0},{0,1,1,1,0,1,1,1,0,1,0}};
-    public static int[][] galabugs = {{0,1,1,1,0,0,0,1},{1,0,0,0,1,1,1,0,1,1},{1,0,1,1,0,0,1,1,1,0},{0,1,1,1,0,1,1,1,0,1,0}};
+    public static int[][] rockets = {{0, 1, 1, 1, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1, 1, 0, 1, 1}, {1, 0, 1, 1, 0, 0, 1, 1, 1, 0}, {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0}};
+    public static int[][] galabugs = {{0, 1, 1, 1, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1, 1, 0, 1, 1}, {1, 0, 1, 1, 0, 0, 1, 1, 1, 0}, {0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0}};
     public static char[][] message = {
-            {'A','L','L'},
-            {'G','A','L','A','B','U','G','S'},
-            {'W','E','R','E'},
-            {'D','E','S','T','R','O','Y','E','D'}
+            {'A', 'L', 'L'},
+            {'G', 'A', 'L', 'A', 'B', 'U', 'G', 'S'},
+            {'W', 'E', 'R', 'E'},
+            {'D', 'E', 'S', 'T', 'R', 'O', 'Y', 'E', 'D'}
     };
 
     public static void main(String[] args) {
-        if (rockets.equals(galabugs)) {
-            System.out.println(message.toString());
+        if (Arrays.deepEquals(galabugs, rockets)) {
+            String str = Arrays.deepToString(message);
+            System.out.println(str);
         }
     }
 }

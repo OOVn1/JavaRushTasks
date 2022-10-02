40a08934468f0673080d68721777d8c2833b89d1
OOV <oshchepkov1911@mail.ru>
2022-10-02 16:21:57 +0300

    add solution JavaSyntax from task 0526

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0526/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0526/Solution.java
@@ -3,15 +3,18 @@ package com.javarush.task.pro.task05.task0526;
 /* 
 Атака вирусов
 */
-/*
+
+import java.util.Arrays;
+
 public class Solution {
-    public static int[][] array = {{1, 2, 3, 4}, {4, 5, 6, 7}, {7, 8, 9, 0}};
+    public static int[][] array = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
     public static void main(String[] args) {
         printArray();
 
-        //напишите тут ваш код
-
+        int[] tmp = array[0];
+        array[0] = array[2];
+        array[2] = tmp;
         printArray();
     }
 
@@ -22,4 +25,3 @@ public class Solution {
         System.out.println();
     }
 }
-*/

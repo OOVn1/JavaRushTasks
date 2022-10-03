c12de3d0bcf4202bf6aa4cd69693c67292247b06
OOV <oshchepkov1911@mail.ru>
2022-10-03 22:08:27 +0300

    add solution JavaSyntax from task 0512

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0512/Solution.java
@@ -9,6 +9,14 @@ public class Solution {
     public static int[][][] multiArray = new int[][][]{{{4, 8, 15}, {16}}, {{23, 42}, {}}, {{1}, {2}, {3}, {4, 5}}};
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < multiArray.length; i++) {
+            for (int j = 0; j < multiArray[i].length; j++) {
+                for (int k = 0; k < multiArray[i][j].length; k++) {
+                    System.out.println(multiArray[i][j][k]);
+                }
+
+
+            }
+        }
     }
 }

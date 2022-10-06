6ca17a8cf8df24ee00ffc9cec647612298038c36
OOV <oshchepkov1911@mail.ru>
2022-10-06 19:59:29 +0300

    add solution JavaSyntax from task 0618

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -4,14 +4,11 @@ package com.javarush.task.pro.task05.task0510;
 Треугольный массив
 */
 
-/*public class Solution {
+public class Solution {
 
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
-        for (int i = 0; i < result.length; i++) {
-            result[]
-        }
+        //напишите тут ваш код
     }
 }
-*/

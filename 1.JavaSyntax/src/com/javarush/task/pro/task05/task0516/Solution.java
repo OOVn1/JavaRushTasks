f9e1d63452b90f4a83151fce6c5e127e7fe03a36
OOV <oshchepkov1911@mail.ru>
2022-10-07 19:45:41 +0300

    add solution JavaSyntax from task 0516

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0516/Solution.java
@@ -13,7 +13,13 @@ public class Solution {
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        if (array.length % 2 == 0) {
+            Arrays.fill(array, 0, array.length / 2, valueStart);
+            Arrays.fill(array, array.length / 2, array.length, valueEnd);
+        } else {
+            Arrays.fill(array, 0, array.length / 2 + 1, valueStart);
+            Arrays.fill(array, array.length / 2 + 1, array.length, valueEnd);
+        }
         System.out.println(Arrays.toString(array));
     }
 }

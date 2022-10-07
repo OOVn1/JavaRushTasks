688ae1e65121c3ac95e7acf1766a2b020cc81bf2
OOV <oshchepkov1911@mail.ru>
2022-10-07 18:21:04 +0300

    add solution JavaSyntax from task 0527

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0527/Solution.java
@@ -8,10 +8,11 @@ public class Solution {
     public static int[][] array = new int[5][];
 
     public static void main(String[] args) {
-        //напишите тут ваш код
 
         for (int i = 0; i < array.length; i++) {
+            array[i] = new int[2 * i +1];
             for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i + j;
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();

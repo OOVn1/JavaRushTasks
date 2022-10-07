7759ea0f3a3dc3869a5206b5bad6cf19e9a075f6
OOV <oshchepkov1911@mail.ru>
2022-10-07 18:14:24 +0300

    add solution JavaSyntax from task 0510

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -9,6 +9,13 @@ public class Solution {
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[i + 1];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

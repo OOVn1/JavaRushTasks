95833c9f5c7836cbf6a0c3de8bd83878f094c00a
OOV <oshchepkov1911@mail.ru>
2022-10-09 14:04:27 +0300

    add solution JavaSyntax from task 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(double a){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(long a){
+
+    }
+    public static void universalMethod(char a){
+
+    }
+    public static void universalMethod(String a){
+
+    }
+    public static void universalMethod(double b, double c){
+
+    }
+    public static void universalMethod(int b, int c){
+
+    }
+    public static void universalMethod(char b, char c){
+
+    }
+    public static void universalMethod(String b, String c){
+
+    }
+    public static void universalMethod(long b, long c){
+
+    }
+}

d81e4332131cbdd921eddf23f9ed245a1de41101
OOV <oshchepkov1911@mail.ru>
2022-10-17 21:56:13 +0300

    add solution JavaSyntax from task 0321

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0321/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0321/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Чтобы вернуть контроль над складом введите пароль:");
 
-        if ("QWERTY".equals("qwerty")) {
+        if ("QWERTY".equalsIgnoreCase("qwerty")) {
             System.out.println("Контроль получен");
         } else {
             System.out.println("Пароль неверный");

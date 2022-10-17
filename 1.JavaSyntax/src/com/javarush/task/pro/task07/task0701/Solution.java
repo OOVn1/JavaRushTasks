384c92657dd7bc3a92ac081262bf4bde279d0436
OOV <oshchepkov1911@mail.ru>
2022-10-17 20:02:53 +0300

    add solution JavaSyntax from task 0701;

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    public static int a;
+    public static double b;
+    public static char c;
+    public static short d;
+    public static boolean v;
+    public static long s;
+    public static float g;
+    public static byte t;
+}

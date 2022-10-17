397f6c97f2c8bdfd8fe183de9408d3c6e474b977
OOV <oshchepkov1911@mail.ru>
2022-10-17 20:08:50 +0300

    add solution JavaSyntax from task 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte b = -128;
+    short sh = -32768;
+    int i = -2147483648;
+    long lng = -9223372036854775808L;
+}

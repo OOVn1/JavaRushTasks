c53f38819d615130707b5eafd9fadd37c88d395a
OOV <oshchepkov1911@mail.ru>
2022-10-19 23:09:25 +0300

    add solution JavaSyntax from task 0706

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task07.task0706;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        div(0, 0);
     }
 
     public static void div(double a, double b){

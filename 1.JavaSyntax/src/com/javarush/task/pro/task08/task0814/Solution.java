73b3b72d559ad9c6ca9f7890dfd3f9c3eaf43940
OOV <oshchepkov1911@mail.ru>
2022-10-28 00:03:58 +0300

    add solution JavaSyntax from task 0816

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task08.task0814;
+
+/* 
+Флаги
+*/
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        //напишите тут ваш код
+        return false;
+    }
+}

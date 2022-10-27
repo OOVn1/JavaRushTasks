350ca131a82c0ac499e36a300d5556d81608a7ac
OOV <oshchepkov1911@mail.ru>
2022-10-27 23:33:15 +0300

    add solution JavaSyntax from task 0808

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task08.task0820;
+
+/* 
+Дубликаты на корабле
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        com.javarush.task.pro.task08.task0820.dup3.Diego diego = new com.javarush.task.pro.task08.task0820.dup3.Diego();
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/dup3/Diego.java
@@ -0,0 +1,4 @@
+package com.javarush.task.pro.task08.task0820.dup3;
+
+public class Diego {
+}

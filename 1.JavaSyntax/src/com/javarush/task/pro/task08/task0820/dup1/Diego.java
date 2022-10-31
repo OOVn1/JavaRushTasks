e5d5b8885af79f1c43a5ab5ee8c9c629922e689e
OOV <oshchepkov1911@mail.ru>
2022-10-31 21:49:40 +0300

    add solution JavaSyntax from task 0820

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/dup1/Diego.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/dup1/Diego.java
@@ -1,5 +1,4 @@
 package com.javarush.task.pro.task08.task0820.dup1;
 
 public class Diego {
-    com.javarush.task.pro.task08.task0820.dup3.Diego diego = new com.javarush.task.pro.task08.task0820.dup3.Diego();
 }
--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0820/dup3/Diego.java
+++
@@ -1,4 +0,0 @@
-package com.javarush.task.pro.task08.task0820.dup3;
-
-public class Diego {
-}

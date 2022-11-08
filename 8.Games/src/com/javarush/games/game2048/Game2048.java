5c3062f3f843d6e5afb86312bce08cf3f09bd3ef
OOV <oshchepkov1911@mail.ru>
2022-11-08 19:14:05 +0300

    add solution JavaSyntax from task 2048.3

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -3,7 +3,13 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
     public void initialize() {
         setScreenSize(SIDE, SIDE);
+
+        createGame();
+    }
+    private void createGame(){
+
     }
 }

045af52bc0f4fe077954d19385d1f258df76499e
OOV <oshchepkov1911@mail.ru>
2022-11-08 19:29:05 +0300

    add solution JavaSyntax from task 2048.4

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -6,10 +6,18 @@ public class Game2048 extends Game {
     private int[][] gameField = new int[SIDE][SIDE];
     public void initialize() {
         setScreenSize(SIDE, SIDE);
-
         createGame();
+        drawScene();
     }
     private void createGame(){
 
+    }
+    private void drawScene(){
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField.length; j++) {
+                setCellColor(i, j, Color.RED);
+            }
+
+        }
     }
 }

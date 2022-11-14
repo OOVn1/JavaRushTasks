b2259f742b8123c44569492302f91ec4792963fa
OOV <oshchepkov1911@mail.ru>
2022-11-14 19:03:45 +0300

    add solution Game2048 past 8/18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -84,7 +84,20 @@ public class Game2048 extends Game {
         }
         return result;
     }
+    private boolean mergeRow(int[] row){
+        boolean result = false;
+        for (int i = 0; i < row.length - 1; i++) {
+            if(row[i] != 0 && row[i] == row[i+1]){
+                row[i] = row[i] + row[i+1];
+                row[i+1] = 0;
+                result = true;
+            }
+        }
 
+
+
+        return result;
+    }
 }
 
 //--module-path "C:\MyProjects\JavaRushTasks\lib\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml

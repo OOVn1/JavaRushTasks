dc09e93ed824d8dee07f63ea90283ff975bba6bc
OOV <oshchepkov1911@mail.ru>
2022-11-20 18:15:18 +0300

    add Solution task 1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        new Africa(400);
+        new Antarctica(4500);
+        new Australia(545445);
+        new Eurasia(234234234);
+        new NorthAmerica(2433424);
+        new SouthAmerica(2342342);
+
+    }
+}

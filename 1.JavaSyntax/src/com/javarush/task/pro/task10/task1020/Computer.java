b9d478bceb347b92cbe25f6123e203666b83fbb4
OOV <oshchepkov1911@mail.ru>
2022-11-22 17:49:07 +0300

    add Solution task 1020

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer(){
+        new SystemUnit();
+        new Mouse();
+        new Monitor();
+        new Keyboard();
+    }
+
+
+}

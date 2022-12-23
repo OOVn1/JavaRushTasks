2ce5f57f8062b8e93f2e839b05527a82c68dab73
OOV <oshchepkov1911@mail.ru>
2022-12-23 18:19:59 +0300

    add Solution task 1315

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Season.java
@@ -0,0 +1,5 @@
+package com.javarush.task.pro.task13.task1315;
+
+public enum Season {
+    WINTER, SPRING, SUMMER, AUTUMN;
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task13.task1315;
+
+/* 
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Season.WINTER);
+        System.out.println(Season.AUTUMN);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.SPRING);
+
+    }
+}

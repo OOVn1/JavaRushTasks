966ecd421c5fd512aef4d9a4aed8ab88eef2b010
OOV <oshchepkov1911@mail.ru>
2022-12-26 19:20:13 +0300

    add Solution task 1508

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1508/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task15.task1508;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Читаем из консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        try (InputStream stream = System.in;
+             InputStreamReader inputStreamReader = new InputStreamReader(stream);
+             BufferedReader reader = new BufferedReader(inputStreamReader);
+             ) {
+            String line = reader.readLine();
+            char[] chars = line.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (i % 2 == 1) {
+                    System.out.print(String.valueOf(chars[i]).toUpperCase());
+                } else {
+                    System.out.print(String.valueOf(chars[i]).toLowerCase());
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+

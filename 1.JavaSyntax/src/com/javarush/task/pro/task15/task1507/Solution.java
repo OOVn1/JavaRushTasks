400195cafeed4e59c726324e3163ef8bb72454e7
OOV <oshchepkov1911@mail.ru>
2023-01-16 16:12:28 +0300

    add Solution task 1507

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1507/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task15.task1507;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+/* 
+Пропускаем не всех
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        List<String> line = Files.readAllLines(Path.of(scanner.nextLine()));
+        for (int i = 0; i < line.size(); i += 2) {
+            System.out.println(line.get(i));
+        }
+
+
+    }
+}
+

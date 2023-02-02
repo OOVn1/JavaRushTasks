08f94147e26f2ed5a8d15f74d4e50f2c6236652c
OOV <oshchepkov1911@mail.ru>
2023-02-02 19:04:27 +0300

    add Solution task 1615

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1615/Solution.java
@@ -0,0 +1,58 @@
+package com.javarush.task.pro.task16.task1615;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+/* 
+Ой как много методов!
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Instant instant = Instant.ofEpochSecond(10);
+        System.out.println(instant);
+        System.out.println(plusMinutes(instant, 2));
+        System.out.println(plusHours(instant, 2));
+        System.out.println(plusDays(instant, 2));
+        System.out.println(minusMinutes(instant, 2));
+        System.out.println(minusHours(instant, 2));
+        System.out.println(minusDays(instant, 2));
+    }
+
+    static public Instant plusMinutes(Instant instant, long minutes) {
+        //напишите тут ваш код
+
+        return instant.plus(minutes, ChronoUnit.MINUTES);
+    }
+
+    static public Instant plusHours(Instant instant, long hours) {
+        //напишите тут ваш код
+
+        return instant.plus(hours, ChronoUnit.HOURS);
+    }
+
+    static public Instant plusDays(Instant instant, long days) {
+        //напишите тут ваш код
+
+        return instant.plus(days, ChronoUnit.DAYS);
+    }
+
+    static public Instant minusMinutes(Instant instant, long minutes) {
+        //напишите тут ваш код
+
+        return instant.minus(minutes, ChronoUnit.MINUTES);
+    }
+
+    static public Instant minusHours(Instant instant, long hours) {
+        //напишите тут ваш код
+
+        return instant.minus(hours, ChronoUnit.HOURS);
+    }
+
+    static public Instant minusDays(Instant instant, long days) {
+        //напишите тут ваш код
+
+        return instant.minus(days, ChronoUnit.DAYS);
+    }
+}

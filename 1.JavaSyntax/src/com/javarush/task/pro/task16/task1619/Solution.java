a8ae1fd16bdf0dd2ba6af34e00d1d0449cc63efd
OOV <oshchepkov1911@mail.ru>
2023-02-05 16:42:33 +0300

    add Solution task 1619

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1619/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task16.task1619;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/* 
+Простой шаблон
+*/
+
+public class Solution {
+
+    static LocalDateTime localDateTime = LocalDateTime.now();
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyyг. Hч.mмин");
+        String str = dtf.format(localDateTime);
+        System.out.println(str);
+    }
+}

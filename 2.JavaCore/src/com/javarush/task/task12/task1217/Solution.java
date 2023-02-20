f7635c571877a9d81c9d0b6294b0a34ec610bce6
OOV <oshchepkov1911@mail.ru>
2023-02-20 15:58:57 +0300

    add Solution task 1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+public interface CanFly{public void name();}
+public interface CanRun{public void name();}
+public interface CanSwim{public void name();}
+
+}

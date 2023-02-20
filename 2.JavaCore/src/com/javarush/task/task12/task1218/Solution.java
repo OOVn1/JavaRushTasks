725d35fbd7e2c23dca90ffa18ac4ddf124fa36fa
OOV <oshchepkov1911@mail.ru>
2023-02-20 16:03:41 +0300

    add Solution task 1218

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1218/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task12.task1218;
+
+/* 
+Есть, летать и двигаться
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanMove {
+        public void move();
+    }
+
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog implements CanMove, CanEat{
+        public void move(){}
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public class Duck implements CanMove, CanEat, CanFly{
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void fly() {
+
+        }
+    }
+
+    public class Car implements CanMove{
+        @Override
+        public void move() {
+
+        }
+    }
+
+    public class Airplane implements CanMove, CanFly{
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void fly() {
+
+        }
+    }
+}

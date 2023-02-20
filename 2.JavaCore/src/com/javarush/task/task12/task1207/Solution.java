ef0628b802a17c2c9e3892374e561ccd41a8cee7
OOV <oshchepkov1911@mail.ru>
2023-02-20 15:29:55 +0300

    add Solution task 1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(100);
+        print((Integer) 10);
+
+    }
+
+    public static void print(int a){}
+    public static void print(Integer a){}
+}

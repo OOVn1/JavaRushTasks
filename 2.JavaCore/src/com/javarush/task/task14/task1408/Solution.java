6875d001ccd272c057748236e7adafdfb1fe9fa2
OOV <oshchepkov1911@mail.ru>
2023-02-20 19:27:52 +0300

    add Solution task 1408

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task14.task1408;
+
+/* 
+Куриная фабрика
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Hen hen = HenFactory.getHen(Country.BELARUS);
+        hen.getCountOfEggsPerMonth();
+    }
+
+    static class HenFactory {
+
+        static Hen getHen(String country) {
+            return switch (country) {
+                case Country.BELARUS -> new BelarusianHen();
+                case Country.MOLDOVA -> new MoldovanHen();
+                case Country.RUSSIA -> new RussianHen();
+                case Country.UKRAINE -> new UkrainianHen();
+                default -> null;
+            };
+        }
+    }
+
+
+}

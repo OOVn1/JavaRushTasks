15d0f4222b4b7c8e1037428392e7e87f2c1d9449
OOV <oshchepkov1911@mail.ru>
2023-03-03 14:13:43 +0300

    add Solution task 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution(){}
+
+    protected Solution(int i){}
+
+    Solution(Double i){}
+
+    private Solution(char i){}
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+
+    public SubSolution() {
+    }
+
+    SubSolution(int i) {
+        super();
+    }
+
+    protected SubSolution(Double i) {
+        super();
+    }
+
+}

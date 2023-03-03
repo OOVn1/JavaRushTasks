c2c31e6040c12267cea8bbffab60bdfcbec0841b
OOV <oshchepkov1911@mail.ru>
2023-03-03 20:38:08 +0300

    add Solution task 1519

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1519/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Разные методы для разных типов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))){
+
+            String str;
+            while (!(str = bufferedReader.readLine()).equals("exit")){
+                try {
+
+                    if (str.contains(".")) {
+                        print(Double.parseDouble(str));
+                    } else {
+                        int number = Integer.parseInt(str);
+                        if (number > 0 && number < 128) {
+                            print((short) number);
+                        } else {
+                            print(number);
+                        }
+                    }
+                }catch (Exception e){
+                    print(str);
+                }
+
+            }
+        }
+    }
+
+    public static void print(Double value) {
+        System.out.println("Это тип Double, значение " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("Это тип String, значение " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("Это тип short, значение " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("Это тип Integer, значение " + value);
+    }
+}

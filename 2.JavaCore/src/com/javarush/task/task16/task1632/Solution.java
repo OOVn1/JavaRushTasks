e6d3ceb8ece967931f219948e5beb0aed093545a
OOV <oshchepkov1911@mail.ru>
2023-03-06 18:44:17 +0300

    add Solution task 1632

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1632/Solution.java
@@ -0,0 +1,95 @@
+package com.javarush.task.task16.task1632;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Клубок
+*/
+
+public class Solution {
+    public static List<Thread> threads = new ArrayList<>(5);
+
+    static {
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public static class Thread1 extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+            }
+        }
+    }
+
+    public static class Thread2 extends Thread {
+        @Override
+        public void run() {
+            try {
+                throw new InterruptedException();
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException");
+            }
+        }
+    }
+
+    public static class Thread3 extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("Ура");
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
+    public static class Thread4 extends Thread implements Message{
+        @Override
+        public void showWarning() {
+            interrupt();
+        }
+
+        @Override
+        public void run() {
+            while (!isInterrupted()){
+
+            }
+        }
+    }
+
+    public static class Thread5 extends Thread {
+        @Override
+        public void run() {
+            int sum = 0;
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));){
+                while (true){
+                    String str = reader.readLine();
+                    if(str.equals("N")){
+                        break;
+                    } else {
+                        sum += Integer.parseInt(str);
+                    }
+                }
+            }catch (IOException e){
+
+            }
+            System.out.println(sum);
+
+
+        }
+    }
+}

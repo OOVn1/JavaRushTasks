0a2297e387859b6fa785529ba4754599eb87b5cc
OOV <oshchepkov1911@mail.ru>
2023-03-17 14:06:00 +0300

    add Solution task 1912

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task19.task1912;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка 2
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+
+        System.out.println(result.replaceAll("te", "??"));
+
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}

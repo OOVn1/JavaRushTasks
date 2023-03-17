d23862acb0bf34160da4abe98dd508bbdc10dba5
OOV <oshchepkov1911@mail.ru>
2023-03-17 14:58:42 +0300

    add Solution task 1922

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) {
+        String file1 = null;
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            file1 = reader.readLine();
+
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+
+        try (BufferedReader in = new BufferedReader(new FileReader(file1))) {
+            while (in.ready()) {
+                String readedString = in.readLine();
+                String[] splitedReadedString = readedString.split(" ");
+
+                int wordCount = 0;
+                for (String s : splitedReadedString) {
+                    if (words.contains(s)) {
+                        wordCount++;
+                    }
+                }
+
+                if (wordCount == 2)
+                    System.out.println(readedString);
+            }
+        } catch (IOException ignore) {
+            /* NOP */
+        }
+    }
+}

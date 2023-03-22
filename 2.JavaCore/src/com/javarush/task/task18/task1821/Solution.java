1697a85aab1afe1c0a64d8c0d751b44c0a8ed938
OOV <oshchepkov1911@mail.ru>
2023-03-22 19:00:13 +0300

    add Solution task 1821

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task18.task1821;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/* 
+Встречаемость символов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        int[] aSCII = new int[128];
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                aSCII[reader.read()]++;
+            }
+        }
+        for (int i = 0; i < aSCII.length; i++) {
+            if (aSCII[i] != 0) {
+                System.out.println((char) i + " " + aSCII[i]);
+            }
+        }
+    }
+}

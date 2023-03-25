836d7054a5b91d4674c296eea8a30b1174f9c9a1
OOV <oshchepkov1911@mail.ru>
2023-03-25 18:03:01 +0300

    add Game Snake 06

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -1,15 +1,32 @@
 package com.javarush.games.snake;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
 
-    private final List<GameObject> snakeParts = new ArrayList<>();
-    public Snake(int x, int y) {
-        snakeParts.add(new GameObject(x,y));
-        snakeParts.add(new GameObject(x + 1,y));
-        snakeParts.add(new GameObject(x + 2,y));
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
 
+
+    private final List<GameObject> snakeParts = new ArrayList<>();
+
+    public Snake(int x, int y) {
+        snakeParts.add(new GameObject(x, y));
+        snakeParts.add(new GameObject(x + 1, y));
+        snakeParts.add(new GameObject(x + 2, y));
     }
-}
+
+    public void draw(Game game) {
+        for (int i = 0; i < snakeParts.size(); i++) {
+            if (i == 0) {
+                game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, HEAD_SIGN);
+            } else {
+                game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, BODY_SIGN);
+            }
+
+            }
+        }
+    }
--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -1,17 +1,17 @@
 package com.javarush.games.snake;
 
-
+import com.javarush.engine.cell.Game;
 import com.javarush.engine.cell.*;
 
  class SnakeGame extends Game {
 
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         createGame();
-        drawScene();
     }
 
     private void drawScene(){
@@ -20,11 +20,12 @@ import com.javarush.engine.cell.*;
                 setCellColor(i, j, Color.BLACK);
             }
         }
+        snake.draw(this);
     }
 
     private void createGame(){
+        snake = new Snake(WIDTH/2, HEIGHT/2);
         drawScene();
-
     }
 
 }

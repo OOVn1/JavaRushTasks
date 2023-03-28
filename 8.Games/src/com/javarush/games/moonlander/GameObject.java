557f8e0d74b626e4693e6a099190ea8a113b8f1b
OOV <oshchepkov1911@mail.ru>
2023-03-28 13:56:47 +0300

    add MoonLanderGame 03

---
+++ 8.Games/src/com/javarush/games/moonlander/GameObject.java
@@ -0,0 +1,12 @@
+package com.javarush.games.moonlander;
+
+public class GameObject {
+    public double x;
+    public double y;
+
+    public GameObject(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}

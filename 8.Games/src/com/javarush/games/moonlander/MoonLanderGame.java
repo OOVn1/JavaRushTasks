1aecc243fef5dbc9d7ea25fa4a920ffb71b1c744
OOV <oshchepkov1911@mail.ru>
2023-03-28 14:11:57 +0300

    add MoonLanderGame 05

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -5,6 +5,8 @@ import com.javarush.engine.cell.*;
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private Rocket rocket;
+
 
     @Override
     public void initialize() {
@@ -18,9 +20,13 @@ public class MoonLanderGame extends Game {
                 setCellColor(i, j, Color.BLACK);
             }
         }
+        rocket.draw(this);
     }
 
     private void createGame(){
+        rocket = new Rocket(WIDTH/2, 0);
         drawScene();
     }
+
+
 }

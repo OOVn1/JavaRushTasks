a965824ccef045a5e1a21aaa1758a9e41cedb3a8
OOV <oshchepkov1911@mail.ru>
2023-04-07 16:48:06 +0300

    add Solution task 1919

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task19.task1919;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+
+/*
+Считаем зарплаты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        TreeMap<String, Double> salary = new TreeMap<>();
+
+        String fileName = args[0];
+
+        try (BufferedReader rd = new BufferedReader(new FileReader(fileName))) {
+            String[] splitedLine;
+            String line;
+            Double currentValue;
+
+            while ((line = rd.readLine()) != null) {
+                splitedLine = line.split(" ");
+                String name = splitedLine[0];
+                double value = Double.parseDouble(splitedLine[1]);
+
+                if (salary.containsKey(name)) {
+                    currentValue = salary.get(name);
+                    salary.put(name, value + currentValue);
+                } else {
+                    salary.put(name, value);
+                }
+            }
+        } catch (IOException ignore) {
+            /*NOP */
+        }
+
+        for (String key : salary.keySet()) {
+            System.out.println(key + " " + salary.get(key));
+        }
+    }
+}

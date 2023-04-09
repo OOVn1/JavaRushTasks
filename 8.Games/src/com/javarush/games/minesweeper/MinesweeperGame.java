0f57ce86c7644ad291b79c3cfb0fb8ee57b8a085
OOV <oshchepkov1911@mail.ru>
2023-04-09 14:32:03 +0300

    add MinesweeperGame 09

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -71,14 +71,21 @@ public class MinesweeperGame extends Game {
 
     private void openTile(int x, int y) {
         GameObject gameObject = gameField[y][x];
-
+        gameObject.isOpen = true;
+        setCellColor(x, y, Color.GREEN);
         if (gameObject.isMine) {
             setCellValue(gameObject.x, gameObject.y, MINE);
+        } else if (gameObject.countMineNeighbors == 0) {
+            setCellValue(gameObject.x, gameObject.y, "");
+            List<GameObject> neighbors = getNeighbors(gameObject);
+            for (GameObject neighbor : neighbors) {
+                if (!neighbor.isOpen) {
+                    openTile(neighbor.x, neighbor.y);
+                }
+            }
         } else {
             setCellNumber(x, y, gameObject.countMineNeighbors);
         }
-        gameObject.isOpen = true;
-        setCellColor(x, y, Color.GREEN);
     }
 
     @Override

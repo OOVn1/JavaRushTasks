6dd5b60d9db8b5026f6c3e22479dd1d9b6ea519c
OOV <oshchepkov1911@mail.ru>
2023-04-14 12:10:11 +0300

    add Solution task 1927

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task19.task1927;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Контекстная реклама
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        testString.printSomething();
+
+        System.setOut(defaultPrintStream);
+
+        String[] byteArray = byteArrayOutputStream.toString().split("\\n");
+        for (int i = 0; i < byteArray.length; i++) {
+            System.out.println(byteArray[i]);
+            if (i % 2 != 0)
+                System.out.println("JavaRush - курсы Java онлайн");
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}

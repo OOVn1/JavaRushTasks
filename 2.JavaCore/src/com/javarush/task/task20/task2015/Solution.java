34cd3091b359bd766a0b062113db639914a9ed6a
OOV <oshchepkov1911@mail.ru>
2023-04-23 13:32:07 +0300

    add Solution task 2015

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task20.task2015;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации
+*/
+
+public class Solution implements Serializable, Runnable{
+    private transient Thread runner;
+    private int speed;
+
+    public Solution(int speed) {
+        this.speed = speed;
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public void run() {
+        // do something here, doesn't matter what
+    }
+
+    /**
+     * Переопределяем сериализацию.
+     * Для этого необходимо объявить методы:
+     * private void writeObject(ObjectOutputStream out) throws IOException
+     * private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+     * Теперь сериализация/десериализация пойдет по нашему сценарию :)
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        Thread thread = new Thread(this);
+        thread.start();
+        in.defaultReadObject();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

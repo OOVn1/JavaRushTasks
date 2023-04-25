58e735495c5b5958a3df5864098f266647d562bf
OOV <oshchepkov1911@mail.ru>
2023-04-25 11:16:44 +0300

    add Solution task 2113.2

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,6 +1,15 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hippodrome {
+    private List<Horse> horses = new ArrayList<>();
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
 
     }

103fa47e0dc2deaa0998d14bc630aa3dbdda11bd
OOV <oshchepkov1911@mail.ru>
2023-04-25 11:18:41 +0300

    add Solution task 2113.3

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Hippodrome {
     private List<Horse> horses = new ArrayList<>();
 
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
     public List<Horse> getHorses() {
         return horses;
     }

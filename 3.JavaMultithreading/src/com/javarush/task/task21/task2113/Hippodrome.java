f07a0872162a123f96a799895694d4ad9e92be6a
OOV <oshchepkov1911@mail.ru>
2023-04-28 13:50:44 +0300

    add Solution task 2113.15

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -52,6 +52,19 @@ public class Hippodrome {
         System.out.println();
         System.out.println();
         System.out.println();
-
     }
+
+    public Horse getWinner() {
+        Horse result = horses.get(0);
+        for (Horse horse : horses) {
+            if (horse.getDistance() > result.getDistance())
+                result = horse;
+        }
+        return result;
+    }
+
+    public void printWinner() {
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+
 }

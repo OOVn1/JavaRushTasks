357be0f036c063826ecdab89eda801c852bc9986
OOV <oshchepkov1911@mail.ru>
2023-04-28 20:03:07 +0300

    add Solution task 2208

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> paramsMap = new LinkedHashMap<>();
+        paramsMap.put("name", "Ivanov");
+        paramsMap.put("country", "Ukraine");
+        paramsMap.put("city", "Kiev");
+        paramsMap.put("age", null);
+
+        System.out.println(getQuery(paramsMap));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder queryBuilder = new StringBuilder();
+        for (String s : params.keySet()) {
+            String value = params.get(s);
+            if (value == null) {
+                continue;
+            }
+            if (queryBuilder.toString().length() != 0) {
+                queryBuilder.append(" and ");
+            }
+            queryBuilder.append(s + " = '" + value + "'");
+        }
+        return queryBuilder.toString();
+    }
+}

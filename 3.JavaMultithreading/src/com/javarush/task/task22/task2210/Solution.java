d43d626b09f526e9dad0468b84ce6239b9fa5797
OOV <oshchepkov1911@mail.ru>
2023-04-30 17:10:43 +0300

    add Solution task 2210

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/* 
+StringTokenizer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] strings = new String[tokenizer.countTokens()];
+        int i = 0;
+        while (tokenizer.hasMoreTokens()){
+            strings[i] = tokenizer.nextToken(delimiter);
+            i++;
+        }
+        return strings;
+    }
+}

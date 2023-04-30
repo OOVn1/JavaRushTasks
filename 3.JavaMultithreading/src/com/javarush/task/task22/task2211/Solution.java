dcbbecdb6669037afe27899fdde1802b22a638eb
OOV <oshchepkov1911@mail.ru>
2023-04-30 17:12:50 +0300

    add Solution task 2211

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/* 
+Смена кодировки
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), "Windows-1251"));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
+
+            String s;
+            while ((s = reader.readLine()) != null) {
+                writer.write(s + "\n");
+            }
+        }
+    }
+}

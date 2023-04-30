bf38fd3a7cbcd09d5f8add00e0e03f80fc02fbe4
OOV <oshchepkov1911@mail.ru>
2023-04-30 17:16:47 +0300

    add Solution task 2212

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task22.task2212;
+
+/* 
+Проверка номера телефона
+*/
+
+public class Solution {
+    public static boolean checkTelNumber(String telNumber) {
+        if (telNumber == null) {
+            return false;
+        }
+        return (telNumber.matches("^\\+(\\d[()]?){12}$") || telNumber.matches("^([()]?\\d){10}$"))
+                && telNumber.matches("^(\\+)?(\\d+)?(\\(\\d{3}\\))?\\d+$");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

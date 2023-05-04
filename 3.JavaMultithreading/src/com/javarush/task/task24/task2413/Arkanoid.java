c726168d51185e867368932637a1d1f316947dbf
OOV <oshchepkov1911@mail.ru>
2023-05-04 20:23:06 +0300

    add Solution task 2413.5

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -3,9 +3,10 @@ package com.javarush.task.task24.task2413;
 import java.util.List;
 
 public class Arkanoid {
-
     private int width;
     private int height;
+    public static Arkanoid game;
+
     private Ball ball;
     private Stand stand;
     private List<Brick> bricks;
@@ -15,44 +16,52 @@ public class Arkanoid {
         this.height = height;
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public Ball getBall() {
         return ball;
-    }
-
-    public Stand getStand() {
-        return stand;
-    }
-
-    public List<Brick> getBricks() {
-        return bricks;
     }
 
     public void setBall(Ball ball) {
         this.ball = ball;
     }
 
+    public Stand getStand() {
+        return stand;
+    }
+
     public void setStand(Stand stand) {
         this.stand = stand;
     }
 
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
     public void setBricks(List<Brick> bricks) {
         this.bricks = bricks;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    void run() {
+
+    }
+
+    void move() {
+
     }
 
     public static void main(String[] args) {

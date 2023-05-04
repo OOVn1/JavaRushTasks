f7af885f6a120e644f74e1db01d91e6feefb39cc
OOV <oshchepkov1911@mail.ru>
2023-05-04 20:37:15 +0300

    add Solution task 2413.11

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -35,18 +35,25 @@ public class Canvas {
         this.matrix = matrix;
     }
 
+    /**
+     * Печатаем переданную фигуру в указанных координатах цветом c.
+     * Если переданный массив содержит единицы, то на холсте им будут соответствовать символы - с.
+     */
     void drawMatrix(double x, double y, int[][] matrix, char c) {
         int height = matrix.length;
         int width = matrix[0].length;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (matrix[i][j] != 0)
+                if (matrix[i][j] == 1)
                     setPoint(x + j, y + i, c);
             }
         }
     }
 
+    /**
+     * Ставим одну точку на холсте с координатами (x,y) и цветом - c.
+     */
     void setPoint(double x, double y, char c) {
         int x0 = (int) Math.round(x);
         int y0 = (int) Math.round(y);
@@ -54,5 +61,26 @@ public class Canvas {
         if (x0 < 0 || x0 >= matrix[y0].length) return;
 
         matrix[y0][x0] = c;
+    }
+
+    void print() {
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 }

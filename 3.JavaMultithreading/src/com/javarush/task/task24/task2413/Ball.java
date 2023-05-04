265917115f167fc03c75313c1f051d79360d9d4f
OOV <oshchepkov1911@mail.ru>
2023-05-04 20:33:46 +0300

    add Solution task 2413.8

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,4 +4,14 @@ public class Ball extends BaseObject {
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
+
+    @Override
+    void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    void move() {
+
+    }
 }

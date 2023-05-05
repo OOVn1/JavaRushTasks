4603c86959f7f3634f35ac46d16aebf6cf7b036f
OOV <oshchepkov1911@mail.ru>
2023-05-05 15:37:14 +0300

    add Solution task 2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void testMethod() {
+
+    }
+
+    public void testMethod2(int i) {
+
+    }
+}

eb0fd091b2c4e1bb4d76b8085e773aa2fd348197
OOV <oshchepkov1911@mail.ru>
2023-05-08 14:45:29 +0300

    add Solution task 2104

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if(this == o){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        if(!(o instanceof Solution)){
+            return false;
+        }
+        Solution solution = (Solution) o;
+        //if(this.first.equals(solution.first))
+        return Objects.equals(this.first, solution.first) && Objects.equals(this.last, solution.last);
+
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, last);
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}

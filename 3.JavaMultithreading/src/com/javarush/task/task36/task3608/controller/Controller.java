220b838f738ad3f8b4a53ec8d8a38f6b0e2e7cde
OOV <oshchepkov1911@mail.ru>
2023-06-21 21:48:53 +0300

    add Solution task 3608.2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task36.task3608.controller;
+
+import com.javarush.task.task36.task3608.model.Model;
+
+public class Controller {
+    private Model model;
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+    public void onShowAllUsers(){
+        model.loadUsers();
+    }
+}

69fa7fedabbb8b2350df57e31525e974244abe7e
OOV <oshchepkov1911@mail.ru>
2023-07-10 10:47:15 +0300

    add Solution task 3008.6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            ConsoleHelper.writeMessage("Чат запущен");
+            while (true){
+                Socket accept = serverSocket.accept();
+                Handler handler = new Handler(accept);
+                handler.start();
+            }
+        }catch (IOException e){
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске сервера");
+        }
+    }
 
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }

c1846a4e70f7a189f0ba61ef9f802bc3c92b2b83
OOV <oshchepkov1911@mail.ru>
2023-07-18 15:30:50 +0300

    add Solution task 3008.19

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task30.task3008.client;
 
-public class BotClient extends Client{
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class BotClient extends Client {
     @Override
     protected SocketThread getSocketThread() {
         return new BotSocketThread();
@@ -20,7 +26,42 @@ public class BotClient extends Client{
         new BotClient().run();
     }
 
-    public class BotSocketThread extends SocketThread{
+    public class BotSocketThread extends SocketThread {
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+            if (message.contains(":")) {
+                String[] split = message.split(":");
+                String name = split[0];
+                String text = split[1].trim();
+                String format = null;
+                if (text.equals("дата")) {
+                    format = "d.MM.YYYY";
+                } else if (text.equals("день")) {
+                    format = "d";
+                } else if (text.equals("месяц")) {
+                    format = "MMMM";
+                } else if(text.equals("год")){
+                    format = "yyyy";
+                }else if(text.equals("время")){
+                    format = "H:mm:ss";
+                }else if(text.equals("час")){
+                    format = "H";
+                }else if(text.equals("минуты")){
+                    format = "m";
+                }else if(text.equals("секунды")){
+                    format = "s";
+                }
+                if(format != null){
+                    sendTextMessage("Информация для " + name + ": " + new SimpleDateFormat(format).format(new GregorianCalendar().getTime()));
+                }
+            }
+        }
     }
 }

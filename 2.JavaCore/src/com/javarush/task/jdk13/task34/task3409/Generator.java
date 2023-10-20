bfc903add7032125a86c746d4002d57852b5a47d
OOV <oshchepkov1911@mail.ru>
2023-10-20 19:30:43 +0300

    add Solution task 3409

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task34/task3409/Generator.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task34.task3409;
+
+import java.lang.reflect.Constructor;
+
+public class Generator<T> {
+
+    private Class<T> aClass;
+
+    public Generator(Class<T> aClass) {
+        this.aClass = aClass;
+    }
+
+    T newInstance() throws Exception {
+        Constructor<T> declaredConstructor = (Constructor<T>) aClass.getDeclaredConstructors()[0];
+        int parameterCount = declaredConstructor.getParameterCount();
+        Object[] objects = new Object[parameterCount];
+        return declaredConstructor.newInstance(objects);
+    }
+}

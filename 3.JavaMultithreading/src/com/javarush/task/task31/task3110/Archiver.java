40516f5c91aeef1ec309dbc901d9e516d4662ad8
OOV <oshchepkov1911@mail.ru>
2023-10-27 21:02:35 +0300

    add Solution task 3110.1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task31.task3110;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Archiver {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите путь для архивации");
+        Scanner scanner = new Scanner(System.in);
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(scanner.nextLine()));
+        System.out.println("Введите путь к файлу, который необходимо архивировать");
+        zipFileManager.createZip(Paths.get(scanner.nextLine()));
+    }
+}

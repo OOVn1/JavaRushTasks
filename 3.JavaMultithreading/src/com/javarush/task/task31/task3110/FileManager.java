b4fbc1145e3b583bf0410af99935601d9b6a9e4e
OOV <oshchepkov1911@mail.ru>
2023-11-09 20:42:09 +0300

    add Solution task 3110.10

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -8,13 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
-
     private Path rootPath;
     private List<Path> fileList;
 
     public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new ArrayList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
 
@@ -22,17 +21,20 @@ public class FileManager {
         return fileList;
     }
 
-    public Path getRootPath() {
-        return rootPath;
-    }
+    private void collectFileList(Path path) throws IOException {
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
 
-    private void collectFileList(Path path) throws IOException{
-        if(Files.isRegularFile(path)){
-            fileList.add(rootPath.relativize(path));
-        } else if (Files.isDirectory(path)) {
-            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)){
-                for (Path newPath : directoryStream) {
-                    collectFileList(newPath);
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
             }
         }

2c52566f9214e337f45dc4905f502c49a0d019b9
OOV <oshchepkov1911@mail.ru>
2023-11-13 20:14:57 +0300

    add Solution task 3110.12

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -6,9 +6,11 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public abstract class ZipCommand implements Command{
-    public ZipFileManager getZipFileManager() throws Exception{
-        ConsoleHelper.writeMessage("Введите полный путь файла архива");
-        return new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+public abstract class ZipCommand implements Command {
+
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
 }

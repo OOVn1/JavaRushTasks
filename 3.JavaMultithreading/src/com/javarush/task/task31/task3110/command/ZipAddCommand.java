3a46322be0f631c2471237259f4080e03262f1ef
OOV <oshchepkov1911@mail.ru>
2023-11-16 20:55:34 +0300

    add Solution task 3110.18

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipAddCommand.java
@@ -4,19 +4,26 @@ import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.ZipFileManager;
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ZipAddCommand extends ZipCommand{
+public class ZipAddCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
-            ConsoleHelper.writeMessage("Добавление файла в архив");
+            ConsoleHelper.writeMessage("Добавление нового файла в архив.");
+
             ZipFileManager zipFileManager = getZipFileManager();
-            ConsoleHelper.writeMessage("Введите полное имя файла для добавления");
-            zipFileManager.addFile(Paths.get(ConsoleHelper.readString()));
-            ConsoleHelper.writeMessage("Добавление файла в архив завершено");
-        }catch (PathIsNotFoundException e){
-            ConsoleHelper.writeMessage("Файл не найден");
+
+            ConsoleHelper.writeMessage("Введите полное имя файла для добавления:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.addFile(sourcePath);
+
+            ConsoleHelper.writeMessage("Добавление в архив завершено.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Файл не был найден.");
         }
     }
 }

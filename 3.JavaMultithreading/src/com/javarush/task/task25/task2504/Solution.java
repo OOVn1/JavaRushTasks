0a0dccfc99eeb597aadc05aa3faf3d5ef8b3df5c
OOV <oshchepkov1911@mail.ru>
2023-11-21 20:20:35 +0300

    add Solution task 2504

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2504/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task25.task2504;
+
+/* 
+Switch для нитей
+*/
+
+public class Solution {
+    public static void processThreads(Thread... threads) {
+        for (Thread thread : threads) {
+            switch (thread.getState()) {
+                case NEW -> thread.start();
+                case WAITING, TIMED_WAITING, BLOCKED -> thread.interrupt();
+                case RUNNABLE -> thread.isInterrupted();
+                case TERMINATED -> System.out.println(thread.getPriority());
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}

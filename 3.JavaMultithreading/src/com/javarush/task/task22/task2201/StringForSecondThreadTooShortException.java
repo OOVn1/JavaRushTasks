441ffb27dc124be375d5f9c1da5f7786d833db2d
OOV <oshchepkov1911@mail.ru>
2023-11-30 20:06:29 +0300

    add Solution task 2201

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForSecondThreadTooShortException.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForSecondThreadTooShortException.java
@@ -1,4 +1,8 @@
 package com.javarush.task.task22.task2201;
 
 public class StringForSecondThreadTooShortException extends RuntimeException {
+
+    public StringForSecondThreadTooShortException(Throwable cause) {
+        super(cause);
+    }
 }
